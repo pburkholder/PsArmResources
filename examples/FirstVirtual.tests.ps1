9da6f7635ef8c448cdb984ff13eb273633420239
Peter Burkholder <peter.burkholder@gsa.gov>
2017-05-08 14:20:08 -0400

    Switching to example for tests

--- examples/FirstVirtual.tests.ps1
+++ examples/FirstVirtual.tests.ps1
@@ -2,84 +2,44 @@
 Invoke-Pester -Test DevCICDPipelineVA 
 #>
 
-Import-Module "AzureRMAudit" -Force
+Import-Module "PsArmPester" -Force
 
-$ResourceGroupName = 'DevCICDPipelineVA'
-$DeployScriptName  = 'Deploy-DevCiCdPipeline.ps1'
+$ResourceGroupName = 'MyRG'
+$DeployScriptName  = 'FirstVirtual.ps1'
 # next line finds the deploy script relative to this test script:
 $DeployScript = Join-Path ($PSCommandPath | Split-Path -Parent) $DeployScriptName
 
 # Set up test cases
-$ResourceTotalTestCase = 22
+$ResourceTotalTestCase = 5
 $ResourceSummaryTestCases = @( 
     @{
         Resource = "Microsoft.Network/networkInterfaces"
-        Expected = 10
-    },
-    @{
-        Resource = "Microsoft.Storage/storageAccounts"
         Expected = 2
     },
     @{
+        Resource = "Microsoft.Storage/storageAccounts"
+        Expected = 1
+    },
+    @{
         Resource = 'Microsoft.Compute/virtualMachines'
-        Expected = 10
+        Expected = 2
     }
 ) 
 
 $VMTestCases = @(
     @{
-        Name = 'CAZGDSO903D'
-        VmSize = 'Standard_D3_V2'
+        Name = 'MyWebServer'
+        VmSize = 'Standard_DS1_V2'
     },
     @{
-        Name = 'CAZGDSO904D'
-        VmSize = 'Standard_D4_V2'
-    },
-    @{
-        Name = 'CAZGDSO905D'
-        VmSize = 'Standard_D2_V2'
-    },
-    @{
-        Name = 'CAZGDSO906D'
-        VmSize = 'Standard_D4_V2'
-    },
-    @{
-        Name = 'CAZGDSO907D'
-        VmSize = 'Standard_D2_V2'
-    },
-    @{
-        Name = 'CAZGDSO912D'
-        VmSize = 'Standard_D2_V2'
-    },
-    @{
-        Name = 'CAZGDSO913D'
-        VmSize = 'Standard_D2_V2'
-    },
-    @{
-        Name = 'CAZGDSO915D'
-        VmSize = 'Standard_D2'
-        Role = 'SonarQube'
-    },
-    @{
-        Name = 'CAZGDSO916D'
-        VmSize = 'Standard_DS2'
-        Role = 'Nexus'
-    },
-    @{
-        Name = 'CAZGSQL908D'
-        VmSize = 'Standard_D2'
-        Role = 'SonarQubeDB'
+        Name = 'MyDBServer'
+        VmSize = 'Standard_DS1_V2'
     }
-)
+) 
 
 $StorageCases = @(
     @{
-        Name = 'devcicdva'
-        State = $true
-        Replication = 'RAGRS'
-    },
-    @{
-        Name = 'managedbackupstorage'
+        Name = 'myrgdemostorage'
         State = $true
         Replication = 'LRS'
     }
